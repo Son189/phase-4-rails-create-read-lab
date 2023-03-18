3d30f2aac90e6e1f0b1e6e6d6fca4cda1fc99efe
son189 <son.august.7334@gmail.com>
2023-03-18 11:59:36 +0300

    done

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,14 @@
+class PlantsController < ApplicationController
+def index
+    plant = Plant.all
+    render json: plant
+end
+def show
+    plant = Plant.find_by(id: params[:id])
+    render json: plant
+end
+def create
+    plant = Plant.create(params.permit(:name , :image , :price))
+    render json: plant
+end
+end
